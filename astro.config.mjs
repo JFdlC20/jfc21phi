7aa1a9fcf6187fe1065e95e1b8292ba60af312eb
JFdlC20 <jesusfdlc@gmail.com>
2025-12-24 12:56:11 +0100

    test6

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,11 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  // 1. Pon aquí la dirección base de tu usuario
+  site: 'https://jfdlc20.github.io',
 
-  integrations: [sitemap()],
-  site: "https://grunge.pages.dev"
+  // 2. IMPORTANTE: El nombre EXACTO de tu repositorio (con barra al principio)
+  base: '/jfc21phi',
+
+  // ... el resto de tu configuración (integrations, etc.) se queda igual
 });
